a15bb752527bfabc57310246d71750bca77ac0e1
binwantsidhu71 <binwantsidhu71@gmail.com>
2017-04-21 18:35:10 -0400

    first commit

---
+++ custom.js
@@ -0,0 +1,29 @@
+$( function() {
+  $( "#SPANMenu" ).sortable();
+  $( "#SPANMenu" ).disableSelection();
+  $( "#sb_left_subwrap" ).draggable();
+  $( "#sb_left_subwrap" ).droppable();
+  $( "#sb_left_wrap" ).draggable();
+  $( "#TDContent34" ).draggable();
+  $( "#sb_rt_wrap" ).draggable();
+  $( "#sb_rt_subwrap" ).draggable();
+  $( "#TDContent3" ).draggable();
+  $( "#TDContent4" ).draggable();
+  $( "#TitleAndMenu" ).draggable();
+  $( "#DIVMenu" ).draggable();
+  $( "#DIVMenu" ).droppable();
+  $( "#subwrap2" ).draggable();
+  $( "#subwrap2" ).droppable();
+  $( "#subwrap3" ).draggable();
+  $( "#subwrap3" ).droppable();
+  $( "#subwrap4" ).draggable();
+  $( "#subwrap4" ).droppable();
+  $( "#subwrap5" ).draggable();
+  $( "#subwrap5" ).droppable();
+  $("#post_page_detail").draggable();
+  $("#post_page_detail").droppable();
+  $(".sb_SelectedWorkDescription").draggable();
+  $(".sb_SelectedWorkDescription").droppable();
+
+
+} );
